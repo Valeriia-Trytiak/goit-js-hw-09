b8f7b14e5dfed4aaafc3028d0d0ae720bf1a08f6
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-16 13:33:04 +0200

    update task-02

---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+const elements = {
+  input: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
