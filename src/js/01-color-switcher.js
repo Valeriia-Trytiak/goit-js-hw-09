ccd4bbb58a5c01a5672b9a69e1c6030034fb3984
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-16 13:06:08 +0200

    update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const elements = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+elements.start.addEventListener('click', onClickStart);
+elements.stop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  timerId = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  elements.start.disabled = true;
+}
+
+function onClickStop() {
+  clearInterval(timerId);
+  elements.start.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
